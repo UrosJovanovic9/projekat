aafa18814f47e8bcb9d1e85475dd8f7f669109af
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2022-04-25 21:26:20 +0200

    Dodata mapa

--- src/components/Kontakt.jsx
+++ src/components/Kontakt.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 function Kontakt() {
+	// const [ isLoaded ] = useLoadScript({ googleMapsApiKey: 'AIzaSyCp2TeN4jSXWRV1JHlwiP6cbj-jiikvR9M' });
+
+	// function Map() {
+	// 	// return <GoogleMap zoom = {10} center = {} ></GoogleMap>;
+	// }
+
 	return (
 		<div className="kontaktInformacije">
 			<div className="kontaktTekst">
@@ -11,13 +18,13 @@ function Kontakt() {
 					cumque at odit optio obcaecati, nesciunt molestiae nemo culpa veniam tempora enim perferendis
 					aspernatur? Veritatis natus placeat autem esse.
 				</p>
-				{/* <p>
+				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, totam dolorum cumque at odit optio
 					obcaecati, nesciunt molestiae nemo culpa veniam tempora enim perferendis aspernatur? Veritatis natus
 					placeat autem esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, totam dolorum
 					cumque at odit optio obcaecati, nesciunt molestiae nemo culpa veniam tempora enim perferendis
 					aspernatur? Veritatis natus placeat autem esse.
-				</p> */}
+				</p>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, totam dolorum cumque at odit optio
 					obcaecati, nesciunt molestiae nemo culpa veniam tempora enim perferendis aspernatur? Veritatis natus
